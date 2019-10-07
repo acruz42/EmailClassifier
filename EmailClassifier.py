d3b9a609e461fe11c8ef35943532ba243c2cbb8a
acruz42 <adorian@nmsu.edu>
2019-10-07 09:34:57 -0600

    testing smtplib, so we can send emails succesfully

--- EmailClassifier.py
+++ EmailClassifier.py
@@ -1,1 +1,14 @@
-print("hello world")
+import smtplib, ssl
+
+port = 465 # For SSL
+
+sender = "testgr@cs.nmsu.edu"
+receiver = "adorian@nmsu.edu"
+message = "\n\nIf you can see this in your email then this was a success.\n\n"
+
+# Crate a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("cs.nmsu.edu", port, context=context) as server:
+   server.login("testgr@cs.nmsu.edu","alRcKyVmaKuS")
+   server.sendmail(sender, receiver, message)
