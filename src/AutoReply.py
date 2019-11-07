e8c6d6efa5519641af77b8d8cf6a6839b0070b20
acruz42 <adorian@nmsu.edu>
2019-11-06 20:03:34 -0700

    Implemented autoreply to sender; not hardcoded

--- src/AutoReply.py
+++ src/AutoReply.py
@@ -1,15 +1,29 @@
 import smtplib
+import sys
 
 port = 25
 
+text = sys.argv[1]
+
+mail = ""
+condition = False
+for c in text:
+	if c == "<":
+		condition = True
+		continue
+	if c == ">":
+		break
+	if condition:
+		mail = mail + c
+
 sender = "testgr@cs.nmsu.edu"
-receiver = "millerd@nmsu.edu"
+receiver = mail
 subject = "Email Classifier Automated Reply"
 text = "\n\nThis is an automated reply from testgr@cs.nmsu.edu.\n\n"
 
 message = 'Subject: {}\n\n{}'.format(subject, text)
 
 with smtplib.SMTP("smtp.cs.nmsu.edu", port) as server:
-    server.login("testgr", "Dunedain03\\")
-    server.sendmail(sender, receiver, message)
-    server.quit()
+	server.login("testgr", "")
+	server.sendmail(sender, receiver, message)
+	server.quit()
