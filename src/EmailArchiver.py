8a97794a9112585ebde9c2483051694fc61ec947
Jacob Karl <karljacobammon@gmail.com>
2019-12-04 09:03:57 -0700

    added comments

--- src/EmailArchiver.py
+++ src/EmailArchiver.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+#Looks through the archive to find the file with the current largest number and creates a file with a
+#number one larger.
 i = 1
 savedFile = "Email#" + str(i) + ".txt"
 while True:
@@ -9,7 +11,7 @@ while True:
 	i = i + 1
 	savedFile = "Email#" + str(i) + ".txt"
 
-
+#Write the archive content generated in the perl script to the file created in the previous step
 fil = open("/home/groups3/testgr/MergerTest/EmailArchive/"+savedFile,"w")
 fil.write(sys.argv[1])
 fil.close()
--- src/modelLoader.py
+++ src/modelLoader.py
@@ -1,3 +1,5 @@
+#This is a proof of concept script that loads the model generated in the Classifier script and runs some 
+#basic test using it.
 import pickle
 import numpy
 #import sklearn
@@ -9,22 +11,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 '''
 
+#Generate the testing questions
 X = ['This is not a question.', 'Should this one be a question?', 'After I arrive what should I do?']
 print (X)
 
 for i in range(len(X)):
     print (X[i])
 
+#Load the model
 #model = LogisticRegression()
 with open('text_classifier_100', 'rb') as training_model:
     model = pickle.load(training_model)
 
 
+#Fit the testing data to the model and predict the test question group
 vect = CountVectorizer()
 X_vected = vect.fit_transform(X)
 y = model.predict(X_vected)
 y2 = model.predict_proba(X_vected)
 
+#Print results
 for i in range(len(X)):
     print ("X is" + X[i] + ", Predicted " + y[i] + ", Probability " + y2[i])
 
